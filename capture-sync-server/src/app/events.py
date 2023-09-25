67ea6170ab026a0028b5ae9becaa4e1147fa2047
davidsantana06 <davidsantana06@hotmail.com>
2023-09-25 14:11:15 -0300

    feat: add application base

--- capture-sync-server/src/app/events.py
+++ capture-sync-server/src/app/events.py
@@ -1,8 +1,28 @@
 from flask_socketio import emit
+from os import path
+import json
+
 from . import socket_io
+from .constants import UPLOADS_FOLDER_PATH
 
 
-@socket_io.on('mensagem_do_cliente')
-def handle_message(message):
-    print(f'Mensagem recebida do cliente: {message}')
-    emit('resposta_do_servidor', 'OK')
+@socket_io.on('send_capture')
+def handle_message(data: str):
+    try:
+        message = dict(json.loads(data))
+        username = message.get('username')
+        send_at = message.get('send_at')
+        text_message = message.get('blb_image')
+
+        # Salvar o texto em um arquivo de texto no diretório especificado
+        filename = f'{username}_{send_at}.txt'
+        filepath = path.join(UPLOADS_FOLDER_PATH, filename)
+
+        with open(filepath, 'w') as f:
+            f.write(text_message)
+
+        print(f'Mensagem recebida do cliente - Username: {username}, Send_at: {send_at}')
+        emit('server_response', 'OK')
+    except Exception as e:
+        print(f'Erro ao processar a mensagem: {str(e)}')
+        emit('server_respose', 'Erro')
--- capture-sync-server/src/app/routes.py
+++ capture-sync-server/src/app/routes.py
@@ -1,4 +1,3 @@
-from flask import render_template
 from http import HTTPStatus
 from werkzeug.exceptions import HTTPException
 
