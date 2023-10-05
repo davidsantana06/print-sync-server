c1f502f3dc41dddc3597aae3e3d5c3616cba7b17
davidsantana06 <davidsantana06@hotmail.com>
2023-10-05 14:29:32 -0300

    feat: add image download

--- print-sync-server/src/app/views.py
+++ print-sync-server/src/app/views.py
@@ -1,9 +1,15 @@
 from datetime import datetime
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    abort, send_file
+)
 from http import HTTPStatus
-from os import listdir
+from os import (
+    path,
+    listdir
+)
 
-from .constants import DT_FORMAT, UPLOADS_DIRECTORY
+from .constants import DT_FORMAT, IMAGE_EXTENSION, UPLOADS_DIRECTORY
 from .misc import render_template
 
 
@@ -20,7 +26,20 @@ def index():
         if len(file_data) == 2:
             images.append({
                 'author': file_data[-1].split('.')[0],
-                'created_at': datetime.strptime(file_data[0], DT_FORMAT)
+                'created_at': datetime.strptime(file_data[0], DT_FORMAT),
+                'file_name': file_name
             })
 
     return (render_template('index', {'images': images}), HTTPStatus.OK)
+
+
+@main.get('/uploads/<file_name>')
+def uploads(file_name: str):
+    image = path.join(UPLOADS_DIRECTORY, file_name)
+
+    if path.exists(image):
+        response = send_file(image, as_attachment=True), HTTPStatus.OK
+    else:
+        abort(404)
+    
+    return response
