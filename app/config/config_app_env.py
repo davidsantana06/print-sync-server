d908a53ad374fe2745f3f2c8ab296c51d4a45019
davidsantana06 <davidsantana06@hotmail.com>
2023-12-09 18:17:06 -0300

    refactor: remove unecessary folders

--- app/config/config_app_env.py
+++ app/config/config_app_env.py
@@ -1,15 +1,12 @@
 from dotenv import load_dotenv
 from flask import Flask
-from os import environ, path
+from os import environ
 
-from app.constants import ROOT_DIRECTORY
-
-
-ENV_FILE_PATH = path.join(ROOT_DIRECTORY, '..', '.env')
+from app.constants import ENV_FILE
 
 
 def configure_app_env(app: Flask) -> None:
-    load_dotenv(ENV_FILE_PATH)
+    load_dotenv(ENV_FILE)
     app.config.from_mapping({
         'SECRET_KEY': environ.get('SECRET_KEY'),
     })
--- app/constants.py
+++ app/constants.py
@@ -1,12 +1,8 @@
 from os import path
 
 
-ROOT_DIRECTORY = path.abspath(
-    path.join(
-        path.dirname(__file__),
-        '..', '..'
-    )
-)
+ROOT_DIRECTORY = path.abspath(path.join(path.dirname(__file__), '..'))
+ENV_FILE = path.join(ROOT_DIRECTORY, '.env')
 APP_DIRECTORY = path.join(ROOT_DIRECTORY, 'src', 'app')
 UPLOADS_DIRECTORY = path.join(ROOT_DIRECTORY, 'uploads')
 
